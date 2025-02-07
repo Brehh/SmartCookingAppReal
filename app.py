147978252076355e80925d95ef08a6cd1df43c07
Brehh <79217159+Brehh@users.noreply.github.com>
2025-02-07 11:24:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,12 @@ from streamlit import number_input as IlllllIIIIIIll, set_page_config as lol, co
 from google.generativeai import configure as lIllIlIIIIlllI, GenerativeModel as IIIIlIIlllIlIl
 IIIlIlIllIIIIlllll = lllIlIIIIlIIll['API_KEYS']
 
-lol(page_title="Smart Cooking App", page_icon="🍽️", layout="wide", initial_sidebar_state="auto", theme="light")
+lol(
+    page_title="My Streamlit App",
+    page_icon="🧊",  # Optional page icon
+    layout="wide",
+    initial_sidebar_state="expanded", # Optional sidebar state
+)
 
 def lllllllIlIIIIIlIll(lIIIlIIIllIlIlIllI):
     for IllllIlIlIllIIlIIl in IIIlIlIllIIIIlllll:
